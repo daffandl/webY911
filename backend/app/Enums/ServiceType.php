57118f79f506e8f9d0aaf94f7a1f87ea04087530
whodaff <daffandl.1409@gmail.com>
2026-04-24 00:30:58 +0700

    Create ServiceType enum for service types

---
+++ backend/app/Enums/ServiceType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum ServiceType: string {
+    case SERVICE_ONE = 'service_one';
+    case SERVICE_TWO = 'service_two';
+    case SERVICE_THREE = 'service_three';
+}
