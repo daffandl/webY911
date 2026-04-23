b57702880b9f0756856386c26e0275fdfcfaedaf
whodaff <daffandl.1409@gmail.com>
2026-04-24 00:31:23 +0700

    Create CustomerDashboard component for authenticated customers

---
+++ app/components/CustomerDashboard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CustomerDashboard = () => {
+  return (
+    <div>
+      <h1>Customer Dashboard</h1>
+      <div>
+        <h2>Bookings</h2>
+        {/* Display bookings information here */}
+      </div>
+      <div>
+        <h2>Invoices</h2>
+        {/* Display invoices information here */}
+      </div>
+    </div>
+  );
+};
+
+export default CustomerDashboard;
